7b0e2c060b8a840471602a3257796c8bc270530f
Michael-Phillips <Michael-Phillips@users.noreply.github.com>
2025-11-16 17:34:24 -0800

    Update Sensors.jsx

--- src/pages/Sensors.jsx
+++ src/pages/Sensors.jsx
@@ -1,6 +1,6 @@
 function Sensors() {
   return (
-    <div style={{ width: '100%', height: '100%' }}>  {/* Changed from 100vh to 100% */}
+    <div style={{ width: '100%', height: 'calc(100vh - 120px)', minHeight: '600px'  // ADDED: Minimum height for usability}}>  {/* Changed from 100vh to 100% */}
       <iframe
         src="https://michael-phillips.github.io/sensor-dashboard/"
         style={{
